ba5b271d58db29ae1714d95fe782c73a3b3908bb
Marius Foss Volden <marius.f.volden@outlook.com>
2025-12-06 21:23:37 +0100

    starten på venueFrontend

--- Sportsworld/src/contexts/VenueContext.tsx
+++ Sportsworld/src/contexts/VenueContext.tsx
@@ -1,1 +1,9 @@
 //TODO MARIUS
+import { useState, createContext } from "react";
+import type { FC, ReactNode } from "react";
+import type { IVenue } from "../interfaces/IVenue";
+
+export const VenueContext = createContext<IVenue | null>(null);
+
+
+
