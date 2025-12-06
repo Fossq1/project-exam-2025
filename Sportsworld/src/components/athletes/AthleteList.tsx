316382ed97a252dde2dbb99a1f5f10f63ce7e1c9
carolshoaei <carolshoaei@hotmail.com>
2025-12-06 21:21:18 +0100

    Athlete Frontend og Backend

--- Sportsworld/src/components/athletes/AthleteList.tsx
+++ Sportsworld/src/components/athletes/AthleteList.tsx
@@ -1,1 +1,26 @@
-// TODO BDD
+import { useContext } from "react";
+import AthleteItem from "./AthleteItem";
+import { type IAthleteContext } from "../../interfaces/IAthleteContext";
+import { AthleteContext } from "../../contexts/AthleteContext";
+
+const AthleteList = () => {
+  const { athletes } = useContext(AthleteContext) as IAthleteContext;
+
+  const getAthleteJSX = () => {
+    const athleteJSX = athletes.map((athlete, index) => {
+      return <AthleteItem key={"Athlete" + index} athlete={athlete} />;
+    });
+    return athleteJSX;
+  };
+
+  return (
+    <section>
+      <header>
+        <h2>List of all athletes: </h2>
+      </header>
+      <section>{getAthleteJSX()}</section>
+    </section>
+  );
+};
+
+export default AthleteList;
