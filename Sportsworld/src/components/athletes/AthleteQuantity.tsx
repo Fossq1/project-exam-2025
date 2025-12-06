ab0525557852f47d2e13717c795610a93c6d9468
carolshoaei <carolshoaei@hotmail.com>
2025-12-06 21:21:18 +0100

    Athlete Frontend og Backend

--- Sportsworld/src/components/athletes/AthleteQuantity.tsx
+++ Sportsworld/src/components/athletes/AthleteQuantity.tsx
@@ -1,1 +1,15 @@
-// TODO BDD
+import { useContext } from "react";
+import { type IAthleteContext } from "../../interfaces/IAthleteContext";
+import { AthleteContext } from "../../contexts/AthleteContext";
+
+const AthleteQuantity = () => {
+  const { getAthleteQuantity } = useContext(AthleteContext) as IAthleteContext;
+
+  return (
+    <section>
+      <p>Amount of athletes: {getAthleteQuantity()}</p>
+    </section>
+  );
+};
+
+export default AthleteQuantity;
