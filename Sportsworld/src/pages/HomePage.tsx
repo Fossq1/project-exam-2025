38336db21c29eab509199c8da5485acc7a6073e7
Marius Foss Volden <marius.f.volden@outlook.com>
2025-12-06 21:23:37 +0100

    starten på venueFrontend

--- Sportsworld/src/pages/HomePage.tsx
+++ Sportsworld/src/pages/HomePage.tsx
@@ -1,1 +1,11 @@
 // TODO TBD 
+const HomePage = () => {
+    return (
+        <section>
+            <h3>HomePage</h3>
+            <p>Welcome to....</p>
+        </section>
+    )
+}
+
+export default HomePage;
