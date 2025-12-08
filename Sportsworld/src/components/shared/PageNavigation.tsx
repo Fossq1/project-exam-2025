1b537a182289863a0d1a0ad46b3f8d8e87beb9ee
carolshoaei <carolshoaei@hotmail.com>
2025-12-08 21:15:15 +0100

    Får kjørt venues fra hovedskjerm

--- Sportsworld/src/components/shared/PageNavigation.tsx
+++ Sportsworld/src/components/shared/PageNavigation.tsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 
 const PageNavigation = () => {
-    return(
-        <nav>
-            <ul>
-                <li><Link to="/">Hjem</Link></li>
-                <li><Link to="venues">Venues</Link></li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Hjem</Link>
+        </li>
+        <li>
+          <Link to="venues">Venues</Link>
+        </li>
+        <li>
+          <Link to="athletes">Athletes</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default PageNavigation;
