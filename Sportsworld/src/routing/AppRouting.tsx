de64796322b4958f1cf816ea74121cb3e3791f3e
Marius Foss Volden <marius.f.volden@outlook.com>
2025-12-12 17:14:58 +0100

    hm?

--- Sportsworld/src/routing/AppRouting.tsx
+++ Sportsworld/src/routing/AppRouting.tsx
@@ -1,27 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage, VenuePage, AthletePage } from "../pages";
-import PageNavigation from "../components/shared/PageNavigation";
+import { HomePage, VenuePage } from "../pages";
+import PageNavigation from '../components/shared/PageNavigation'
 import FinancePage from "../pages/FinancePage";
 
-
-
-
 const AppRouting = () => {
-  return (
-    <BrowserRouter>
-      <PageNavigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/venues" element={<VenuePage />} />
-        <Route path="/athletes" element={<AthletePage />} />
+    return(
+        <BrowserRouter>
+            <PageNavigation/>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/venues' element={<VenuePage/>} />
                 <Route path='/finance' element={<FinancePage/>}/>
-                <Route path="/athletes" element={<AthletePage />} />
-
-
             </Routes>
         </BrowserRouter>
     );
 };
 
 export default AppRouting;
-
