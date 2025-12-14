8412f4931dac0654d02e001c462bd4be8a8fddb9
Marius Foss Volden <marius.f.volden@outlook.com>
2025-12-14 22:34:49 +0100

    sameSame

--- Sportsworld/src/components/shared/PageNavigation.tsx
+++ Sportsworld/src/components/shared/PageNavigation.tsx
@@ -3,21 +3,15 @@ import { Link } from "react-router-dom";
 const PageNavigation = () => {
   return (
     <nav className="w-auto flex justify-evenly h-12 items-center">
-      <ul>
-        <li>
-          <Link to="/">Hjem</Link>
-        </li>
-        <li>
-          <Link to="venues">Venues</Link>
-        </li>
-        <li>
-          <Link to="athletes">Athletes</Link>
-        </li>
-                <li><Link to="finance">Finance</Link></li>
-            </ul>
-        </nav>
-    )
-}
+      <Link to="/">Hjem</Link>
+
+      <Link to="venues">Venues</Link>
+
+      <Link to="athletes">Athletes</Link>
+
+      <Link to="editathletes">Create own athlete</Link>
+    </nav>
+  );
+};
 
 export default PageNavigation;
-
